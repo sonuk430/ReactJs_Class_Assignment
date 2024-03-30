a33fad022167fefb6da110efa8d222856b346614
Sonu Kumar <kumarsonuk430@gmail.com>
2024-03-31 01:14:19 +0530

    static ui done & show and hide addFriend btn logic is done

--- mini-project/src/Screen/ListOfFriends.js
+++ mini-project/src/Screen/ListOfFriends.js
@@ -1,7 +1,121 @@
-export default function ListOfFriends(){
-    return(
-        <>
-        <div>ListOfFriends</div>
-        </>
-    )
+import { useState } from "react";
+import "./ListOfFriends.css";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function ListOfFriends() {
+    const [showAddFriend,setShowAddFriend] = useState(false);
+
+    function handleShowAddFriend(){
+        setShowAddFriend((show) => !show);
+    }
+
+  return (
+    <>
+      <div className="app">
+        <div className="sidebar">
+          <FriendsList />
+         {showAddFriend && <FormAddFriend />}
+          <Button onClick={handleShowAddFriend} >{showAddFriend ? "Close" : "Add Friend"}</Button>
+        </div>
+        <FormSplitBill />
+      </div>
+    </>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          Tou owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          Owes you {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>Tou and {friend.name} are even</p>}
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function Button({ children ,onClick}) {
+  return <button className="button" onClick={onClick}>{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👭Friend Name</label>
+      <input type="text" />
+
+      <label>🎴Image URL</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill Value</label>
+      <input type="text" />
+
+      <label>🕺 Your Expense</label>
+      <input type="text" />
+
+      <label>👭 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+      <Button>Split Bill</Button>
+    </form>
+  );
 }
